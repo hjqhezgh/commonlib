2c83cd6f6465174fd6835faf33a41451e9a2fdc8
black <huang_jin_qiang@sina.com>
2013-09-17 22:00:53 +0800

    测试多文件UI

---
+++ test1.go
@@ -0,0 +1,19 @@
+// Title：
+//
+// Description:
+//
+// Author:black
+//
+// Createtime:2013-09-17 21:59
+//
+// Version:1.0
+//
+// 修改历史:版本号 修改日期 修改人 修改说明
+//
+// 1.0 2013-09-17 21:59 black 创建文档
+package commonlib
+
+func Test2(){
+	
+}
+
