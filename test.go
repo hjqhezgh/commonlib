0d94ba4613ffe71519e11796d3e3661dcaf58188
black <huang_jin_qiang@sina.com>
2013-09-17 21:58:43 +0800

    测试客户端提交
    测试客户端提交

---
+++ test.go
@@ -0,0 +1,23 @@
+// Title：
+//
+// Description:
+//
+// Author:black
+//
+// Createtime:2013-09-17 21:55
+//
+// Version:1.0
+//
+// 修改历史:版本号 修改日期 修改人 修改说明
+//
+// 1.0 2013-09-17 21:55 black 创建文档
+package commonlib
+
+import (
+	"fmt"
+)
+
+func Test(){
+	fmt.Println(11)
+}
+
